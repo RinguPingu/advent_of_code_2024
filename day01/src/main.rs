ce8f56904b4613082c92b3ab2506f3ee502ebb3a
Love Arvidsson <lovea@playdead.com>
2024-12-09 13:54:28 +0100

    Make iterators more readable

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         .iter()
         .map(|pair| pair.first().unwrap().parse().unwrap())
         .collect();
-    
+
     let mut right_list: Vec<i32> = number_pairs
         .iter()
         .map(|pair| pair.last().unwrap().parse().unwrap())
@@ -22,20 +22,24 @@ fn main() {
     println!("{:?}", left_list);
     println!("{:?}", right_list);
 
-    let distances: Vec<u32> = left_list.iter().zip(&right_list).map(|numbers| numbers.0.abs_diff(*numbers.1)).collect();
+    let distances: Vec<u32> = left_list
+        .iter()
+        .zip(&right_list)
+        .map(|numbers| numbers.0.abs_diff(*numbers.1))
+        .collect();
 
     let summed_distances: u32 = distances.iter().sum();
 
     println!("Summed Distances:\t{}", summed_distances);
 
-    let similarity_scores : Vec<i32> = left_list.iter().map(|num| {
-        num * right_list.iter().filter(|n| *n == num).count() as i32
-    }).collect();
+    let similarity_scores: Vec<i32> = left_list
+        .iter()
+        .map(|num| num * right_list.iter().filter(|n| *n == num).count() as i32)
+        .collect();
 
     println!("{:?}", similarity_scores);
 
-    let summed_similarity_scores : i32 = similarity_scores.iter().sum();
+    let summed_similarity_scores: i32 = similarity_scores.iter().sum();
 
     println!("Summed Similarity Scores:\t{}", summed_similarity_scores);
-
 }
